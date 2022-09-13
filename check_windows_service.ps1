c685fcdfa02519e2d5c6b9c69dda0fb0cd071ec5
xmarzl <96025908+xmarzl@users.noreply.github.com>
2022-09-13 13:16:41 +0200

    Create check_windows_service.ps1

---
+++ check_windows_service.ps1
@@ -0,0 +1,21 @@
+# -- init
+$service_name = $args[0]
+$service_state = "not found"
+
+# -- get service state, format as list, where line > 0 (=no empty lines)
+$service_state = Get-Service | Where-Object {$_.Name -EQ $service_name} | Format-List -Property Status | Out-String -Stream | Where { $_.Trim().Length -gt 0} | Out-String -Stream
+
+# -- replace key + colon
+$service_state = $service_state -replace '.*: ', ''
+if($service_state -EQ "Running") {
+    Write-Output("$service_name is running.")
+    Exit 0
+}
+elseif ($service_state -EQ "Stopped") {
+    Write-Output("$service_name is stopped.")
+    Exit 2
+}
+else {
+    Write-Output("$service_name not found")
+    Exit 1
+}
